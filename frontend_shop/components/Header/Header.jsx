4b52b2479b28724762f8b64127327aa9e2d9cf36
guamy540 <49961360+guamy540@users.noreply.github.com>
2022-08-18 16:56:33 -0700

    edited header page

--- frontend_shop/components/Header/Header.jsx
+++ frontend_shop/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {booya} from '../../assets'
+import {booya, buttons, prints, stickers} from '../../assets'
 import Image from 'next/image'
 import {motion} from 'framer-motion'
 
@@ -11,11 +11,54 @@ const Header = () => {
         initial={{x:-1000}}
         animate={{x: 0}}
         transition={{duration:.5}}
-        className='header-h1-container'
+        className='header-title-container'
         >
-            <h1 className='header-h1' >ODD CAT STUDIO SHOP</h1>
+            <h1 className='header-h1' >Check out our stuff!</h1>
+            <a href={'#prints'}><motion.button 
+            whileHover={{scale: 1.3, }}
+            className='header-cta-button'>
+              Shop Now!</motion.button></a>
         </motion.div>
-        <motion.div className='header-image-container'
+        <div className='header-cta-container'>
+          <a href={`#prints`}>
+          <motion.div whileHover={{scale: 1.3, }} className='header-img-container'>
+            <Image src={prints} 
+            alt = "Odd Cat Studio Intro Gif" 
+            height={250}
+            width={250}
+            className='header-img'></Image>
+            <p className='header-cta-p'>Prints</p>
+          </motion.div></a>
+
+          <a href={'#stickers'}>
+          <motion.div whileHover={{scale: 1.3, }}  className='header-img-container'>
+          <Image src={stickers} 
+          className='header-img'
+            alt = "Odd Cat Studio Intro Gif" 
+            height={250}
+            width={250}></Image>
+            <p>Stickers</p></motion.div></a>
+          
+          <a href={'#buttons'}>
+          <motion.div whileHover={{scale: 1.3, }}  className='header-img-container'>        
+          <Image src={buttons} 
+          className='header-img'
+            alt = "Odd Cat Studio Intro Gif" 
+            height={250}
+            width={250}></Image>
+            <p>Buttons</p></motion.div></a>
+
+          <a href={'#singletons'}>
+          <motion.div whileHover={{scale: 1.3, }}  className='header-img-container'>
+          <Image src={booya} 
+          className='header-img'
+            alt = "Odd Cat Studio Intro Gif" 
+            height={250}
+            width={250}></Image>
+            <p>One of a kinds!</p></motion.div></a>      
+        </div>
+
+       {/* <motion.div className='header-image-container'
            initial={{y:-1000}}
            animate={{y: 0}}
            transition={{delay: .5, duration:.5}}
@@ -25,11 +68,11 @@ const Header = () => {
             alt = "Odd Cat Studio Intro Gif" 
             height={750}
             width={750}  />
-        </motion.div>
+  </motion.div>
         <motion.div className='shop-now-message-container'
         >
             <h2>Here you can buy many of our fantastic products!</h2>
-        </motion.div>
+        </motion.div>*/}
     </div>
   )
 }
--- frontend_shop/components/Layout/Layout.jsx
+++ frontend_shop/components/Layout/Layout.jsx
@@ -28,3 +28,4 @@ const Layout = ({children}) => {
 }
 
 export default Layout
+
